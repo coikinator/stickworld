a5ee1fe49923c6d7f727b11c868bd56cf3785c73
coikinator <bruhlalallalalla@gmail.com>
2026-05-01 23:43:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ players[socket.id] = {
   facing:1,
   moving:false,
   coins: user?.coins || 0,
-  lastReward: p.lastReward
+  lastReward: user?.lastReward ?? Date.now(),
   timeLeft: Math.max(0, 600000 - (Date.now() - (user?.lastReward ?? Date.now()))),
   inGame: data.page === 'game'
 };
